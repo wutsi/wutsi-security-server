fca69b6527331a8726875dd4bbeb513df1d177c2
htchepannou <htchepannou@expedia.com>
2021-09-21 07:05:40 -0400

    update api

--- src/main/kotlin/com/wutsi/platform/security/dto/SearchApplicationResponse.kt
+++ src/main/kotlin/com/wutsi/platform/security/dto/SearchApplicationResponse.kt
@@ -1,5 +1,7 @@
 package com.wutsi.platform.security.dto
 
+import kotlin.collections.List
+
 public data class SearchApplicationResponse(
     public val applications: List<ApplicationSummary> = emptyList()
 )
--- src/main/kotlin/com/wutsi/platform/security/endpoint/SearchApplicationsController.kt
+++ src/main/kotlin/com/wutsi/platform/security/endpoint/SearchApplicationsController.kt
@@ -1,11 +1,13 @@
 package com.wutsi.platform.security.endpoint
 
-import com.wutsi.platform.security.delegate.SearchApplicationsDelegate
+import com.wutsi.platform.security.`delegate`.SearchApplicationsDelegate
 import com.wutsi.platform.security.dto.SearchApplicationResponse
-import org.springframework.web.bind.annotation.CrossOrigin
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestParam
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.`annotation`.CrossOrigin
+import org.springframework.web.bind.`annotation`.GetMapping
+import org.springframework.web.bind.`annotation`.RequestParam
+import org.springframework.web.bind.`annotation`.RestController
+import kotlin.Int
+import kotlin.String
 
 @RestController
 @CrossOrigin(
