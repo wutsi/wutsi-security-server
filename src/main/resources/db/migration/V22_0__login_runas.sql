4a8bbacca441a445283d5f616ac5f3463d61a802
htchepannou <htchepannou@expedia.com>
2021-11-06 20:38:29 -0400

    grant the permission auth-runas to login app

---
+++ src/main/resources/db/migration/V22_0__login_runas.sql
@@ -0,0 +1,5 @@
+INSERT INTO  T_APPLICATION_SCOPE(application_fk, scope_fk)
+    SELECT A.id, S.id FROM T_APPLICATION A, T_SCOPE S
+    WHERE A.name='com.wutsi.wutsi-login' AND S.name IN (
+        'auth-runas'
+    );
