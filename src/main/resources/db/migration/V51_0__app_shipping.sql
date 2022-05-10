c720d6d130619b1e9672ee5335573c40088ae103
wutsi <herve.tchepannou@wutsi.com>
2022-05-10 09:51:00 -0400

    fix sql

--- src/main/resources/db/migration/V51_0__app_shipping.sql
+++ src/main/resources/db/migration/V51_0__app_shipping.sql
@@ -8,5 +8,5 @@ INSERT INTO  T_APPLICATION_SCOPE(application_fk, scope_fk)
     SELECT A.id, S.id FROM T_APPLICATION A, T_SCOPE S
     WHERE A.name='com.wutsi.wutsi-shipping' AND S.name IN (
        'shipping-read',
-       'shipping-manage',
+       'shipping-manage'
     );
