1a412f516fa80a25cb4669ac20ce595c99a854c7
wutsi <herve.tchepannou@wutsi.com>
2022-05-24 06:45:27 -0400

    rename services

--- src/main/resources/db/migration/V54_0__renaming-apps.sql
+++ src/main/resources/db/migration/V54_0__renaming-apps.sql
@@ -1,8 +1,6 @@
-UPDATE T_APPLICATION
-    SET name='com.wutsi.wutsi-login-bff'
-    WHERE name = 'com.wutsi.wutsi-login';
-
-INSERT INTO T_APPLICATION(name, api_key, active, title, security_level, description)
-    VALUES
-        ('com.wutsi.wutsi-shell-bff', uuid_generate_v1(), true, 'Wutsi Shell App', 0, 'Wutsi main app')
-;
+UPDATE T_APPLICATION SET name='com.wutsi.wutsi-contact-service' WHERE name = 'com.wutsi.wutsi-contact';
+UPDATE T_APPLICATION SET name='com.wutsi.wutsi-notification-service' WHERE name = 'com.wutsi.wutsi-notification';
+UPDATE T_APPLICATION SET name='com.wutsi.wutsi-order-service' WHERE name = 'com.wutsi.wutsi-order';
+UPDATE T_APPLICATION SET name='com.wutsi.wutsi-payment-service' WHERE name = 'com.wutsi.wutsi-payment';
+UPDATE T_APPLICATION SET name='com.wutsi.wutsi-security-service' WHERE name = 'com.wutsi.wutsi-security';
+UPDATE T_APPLICATION SET name='com.wutsi.wutsi-shipping-service' WHERE name = 'com.wutsi.wutsi-shipping';
