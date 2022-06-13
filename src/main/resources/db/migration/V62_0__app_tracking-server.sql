e24b9d6be276861d11e6c9d4239b408b0c096296
wutsi <herve.tchepannou@wutsi.com>
2022-06-13 00:46:06 -0400

    add tracking app

---
+++ src/main/resources/db/migration/V62_0__app_tracking-server.sql
@@ -0,0 +1,4 @@
+INSERT INTO T_APPLICATION(name, api_key, active, title, security_level, description)
+    VALUES
+        ('com.wutsi.wutsi-trancing-service', uuid_generate_v1(), true, 'Wutsi Tracking Server', 0, 'Service for tracking users')
+;
