960ee5b290f3bd3bac8c71c455b27010dd01d5ed
wutsi <herve.tchepannou@wutsi.com>
2022-06-20 16:39:51 -0400

    update shipping permission

---
+++ src/main/resources/db/migration/V64_0__shipping_permission.sql
@@ -0,0 +1,6 @@
+INSERT INTO  T_APPLICATION_SCOPE(application_fk, scope_fk)
+    SELECT A.id, S.id FROM T_APPLICATION A, T_SCOPE S
+    WHERE A.name='com.wutsi.wutsi-shipping-service' AND S.name IN (
+        'catalog-read',
+        'tenant-read'
+    );
