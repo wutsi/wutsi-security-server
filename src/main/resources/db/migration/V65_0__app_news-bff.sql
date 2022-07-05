1b1475ea2466d38e31ad03ed8f4de8dd6022591a
wutsi <herve.tchepannou@wutsi.com>
2022-07-05 16:36:53 -0400

    add news app

---
+++ src/main/resources/db/migration/V65_0__app_news-bff.sql
@@ -0,0 +1,5 @@
+INSERT INTO T_APPLICATION(name, api_key, active, title, security_level, description)
+    VALUES
+        ('com.wutsi.wutsi-news-bff', uuid_generate_v1(), true, 'Wutsi News Application', 0, 'News reader')
+;
+
